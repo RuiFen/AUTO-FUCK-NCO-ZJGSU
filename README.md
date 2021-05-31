4389dc36d9110300b5fa3a85865aa972d801bd9b
yujianke100 <734962820@qq.com>
2021-05-31 18:33:35 +0800

    alpha version

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# AUTO-FXCU-NCO-ZJGSU
+# FUCK NCO ZJGSU
+
 浙江工商大学 云战役 自动报送 Github全自动版
+
+魔改自[FUCK NCO ZJGSU](https://github.com/Hukeqing/FUCK-NCO-ZJGSU)。利用Github的Actions，让你不用准备任何东西，只需要白嫖微软的服务器，就能实现每天自动签到。
+
+默认设置凌晨十二点半和早八点各打卡一次。
+
+## 使用方法
+
+1. 点右上角Fork本项目。
+
+2. 进入项目的设置，添加签到用户信息：
+     - Settings -> Secrets -> New reponsitory secret
+     - Name: users
+     - Value: 
+        ````Json
+        //Example
+        [
+            {
+                "name": "your_student_num",
+                "psswd": "your_password"
+            },
+            {
+                "name": "your_student_num",
+                "psswd": "your_password",
+                "locationInfo": "浙江省杭州市金沙港生活区"
+            }
+        ]
+        ````
+
+3. 进入Actions，允许运行
+
+## ~~本体不是我写的，出问题别打我，传送门再给你一次，打[hkq](https://github.com/Hukeqing/FUCK-NCO-ZJGSU)去。~~
+
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import uuid
+import re
+import requests
+import datetime
+import time
+import os
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
+
+users = os.environ["users"]
+users = json.loads(users)
+
+print('当前时间：', datetime.datetime.now())
+
+for user in users:
+    s = requests.session()
+    s.post('https://nco.zjgsu.edu.cn/login', data=user, headers=header)
+    res = s.get('https://nco.zjgsu.edu.cn/', headers=header)
+    content = str(res.content, encoding='utf-8')
+    if re.search('当天已报送!', content):
+        print(datetime.datetime.now().strftime('%Y-%m-%d'), '报送情况： *主动报送*')
+        continue
+    data = {}
+    for item in re.findall(R'<input.+?>', content):
+        key = re.search(R'name="(.+?)"', item).group(1)
+        value = re.search(R'value="(.*?)"', item).group(1)
+        check = re.search(R'checked', item)
+        if key not in data.keys():
+            data[key] = value
+        elif check is not None:
+            data[key] = value
+    for item in re.findall(R'<textarea.+?>', content):
+        key = re.search(R'name="(.+?)"', item).group(1)
+        data[key] = ''
+    # 为了安全起见，这里还是推荐加上大致的地址和uuid值，虽然经过测试，不填写也可以正常使用
+    # ---------------安全线-------------#
+    data['uuid'] = str(uuid.uuid1())
+    if('locationInfo' not in data):
+        data['locationInfo'] = '浙江省杭州市金沙港生活区'
+    # ---------------安全线-------------#
+    res = s.post('https://nco.zjgsu.edu.cn/', data=data, headers=header)
+    print(datetime.datetime.now().strftime('%Y-%m-%d'), '报送情况：', '报送成功' if
+          re.search('报送成功', str(res.content, encoding='utf-8')) is not None else '报送失败！！！！！')
+    time.sleep(10)
