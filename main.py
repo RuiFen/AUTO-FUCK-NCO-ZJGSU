c24185c9e8cb72791d68b52f736a78a046482b2e
yujianke100 <734962820@qq.com>
2021-05-31 19:41:08 +0800

    fix bug

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ header = {
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
 
 users = os.environ["users"]
-
-users = json.loads(json.dumps(users))
+users = json.loads(users)
 
 print('当前时间：', datetime.datetime.now())
 
